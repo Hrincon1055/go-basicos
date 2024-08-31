3ad182486d09a3f97dbd9cea51e91bbb70123973
Henry Rincon <43394021+Hrincon1055@users.noreply.github.com>
2024-08-30 20:18:04 -0500

    variables

--- variables/enteras.go
+++ variables/enteras.go
@@ -6,8 +6,15 @@ func MostrarEnteros() {
 	var intcomun int
 	intde32 := int32(10)
 	intde64 := int64(100)
+	var firtName, lastName string
+	var message = "Hola mundo"
+	firtName = "Henry"
+	lastName = "rincon"
 	fmt.Println("incomun = ", intcomun)
 	fmt.Println("intde32 = ", intde32)
 	fmt.Println("intde64 = ", intde64)
+	fmt.Println("firtName = ", firtName)
+	fmt.Println("lastName = ", lastName)
+	fmt.Println("message = ", message)
 
 }
